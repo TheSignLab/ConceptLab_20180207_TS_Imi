6afd33a359022a92b17786a61f7ee3461697b855
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-02-28 17:47:23 -0500

    Update Con UWA Mobile

--- es/webProject0_Santimaye/ver_desktop/comp/cmp4_description/script.js
+++ es/webProject0_Santimaye/ver_desktop/comp/cmp4_description/script.js
@@ -1,34 +1,23 @@
 function updateDescription(k, type) {
-
-
-
     if (k == 3 && type == "on") {
-
         anim_intro_desc();
-
-    } else {
-
-        
     }
-
+    else {}
 }
-
 
 function anim_intro_desc() {
-    
     $(".content ul li").css({
-        "left": "0px",
-    });
+        "left": "0px"
+    , });
     $(".Description-Left").height('100vh');
     $(".titles").css({
-        "left": "0px",
-    });
-  
+        "left": "0px"
+    , });
     setTimeout(function () {
-          $(".hbar").fadeIn(500);
+        $(".hbar").fadeIn(500);
         $(".Description-Right .content p").css({
-            "top": "0px",
-        });
+            "top": "0px"
+        , });
         setTimeout(function () {
             setTimeout(function () {
                 $($(".content ul li")[0]).fadeIn(300);
@@ -48,24 +37,19 @@ function anim_intro_desc() {
         }, 700);
     }, 500);
 }
-
-
-
-
 
 function anim_ending_desc() {
-    
     $(".content ul li").css({
-        "left": "0px",
-    });
+        "left": "0px"
+    , });
     $(".Description-Left").height('100vh');
     $(".titles").css({
-        "left": "0px",
-    });
+        "left": "0px"
+    , });
     setTimeout(function () {
         $(".Description-Right .content p").css({
-            "top": "0px",
-        });
+            "top": "0px"
+        , });
         setTimeout(function () {
             setTimeout(function () {
                 $($(".content ul li")[0]).fadeIn(300);
@@ -86,34 +70,19 @@ function anim_ending_desc() {
     }, 500);
 }
 
-
-
-
-
-function updateCover(k,kp,type){
-
-    if(type=="onleave" && kp == 3 && k == 4){
-        
-       $(".cmp_Cover .buttons ").fadeOut(0,function(){
-          $(".cmp_Cover .wrapper").removeClass("padded");  
+function updateCover(k, kp, type) {
+    if (type == "onleave" && kp == 3 && k == 4) {
+        $(".cmp_Cover .buttons ").fadeOut(0, function () {
+            $(".cmp_Cover .wrapper").removeClass("padded");
         });
     }
-    
-    
-    if(type=="onleave" && kp == 4 && k == 3){
-        
-        
-        $(".cmp_Cover .buttons ").fadeOut(0,function(){
-          $(".cmp_Cover .wrapper").removeClass("padded");  
+    if (type == "onleave" && kp == 4 && k == 3) {
+        $(".cmp_Cover .buttons ").fadeOut(0, function () {
+            $(".cmp_Cover .wrapper").removeClass("padded");
         });
-
-
     }
-    
-       if(type=="onload" && k == 4){
-        
+    if (type == "onload" && k == 4) {
         $(".cmp_Cover .wrapper").addClass("padded");
         $(".cmp_Cover .buttons ").fadeIn();
     }
-    
 }
--- es/webProject1_Thesign/ver_desktop/comp/cmp2_portada/script.js
+++ es/webProject1_Thesign/ver_desktop/comp/cmp2_portada/script.js
@@ -3,16 +3,9 @@ vid.onended = function () {
     $.fn.fullpage.moveSectionDown();
 };
 
-
 function hideThisModal() {
-
-
     $(".mdl_PortadaText").removeClass("onDisplay");
 }
-
-
-
-
 //
 var modalText = $(".mdl_PortadaText")[0];
 if (modalText.addEventListener) {
@@ -27,37 +20,21 @@ else {
 }
 
 function ModalWheelHandler(e) {
-
     if (CurrentSection == 1) {
         // cross-browser wheel delta
         var e = window.event || e; // old IE support
-
-
         var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-
         if (delta == -1) {
-
             $(modalText).removeClass("onDisplay");
             $(".cmp_Portada img").hide();
             $("footer").fadeOut();
             $(".cmp_Portada video").show();
             $(".cmp_Portada video")[0].play();
-
-
+            $("nav").hide();
             $.fn.fullpage.setAllowScrolling(false);
         }
     }
-
 }
-
-
-
-
-
-
-
-
-
 var portadaTComp = $(".cmp_Portada")[0];
 if (portadaTComp.addEventListener) {
     // IE9, Chrome, Safari, Opera
@@ -70,40 +47,30 @@ else {
     portadaTComp.attachEvent("onmousewheel", PortadaWheelHandler);
 }
 
-
 function PortadaWheelHandler(e) {
     if (CurrentSection == 1) {
         // cross-browser wheel delta
         var e = window.event || e; // old IE support
-
-
         var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-
         if (delta == 1) {
-
             $(modalText).addClass("onDisplay");
             $(".cmp_Portada img").show();
             $("footer").fadeIn();
+            $("nav").show();
             $(".cmp_Portada video").hide();
             $(".cmp_Portada video")[0].pause();
-
-
             $.fn.fullpage.setAllowScrolling(false);
-        } else {
+        }
+        else {
             $.fn.fullpage.moveTo(2);
             $.fn.fullpage.setAllowScrolling(true);
         }
-        
-        
-        
-    } else {
-
     }
 }
 
-function updatePlayVideo(k){
-    if(k == 1){
+function updatePlayVideo(k) {
+    if (k == 1) {
         $(".cmp_Portada video").show();
-            $(".cmp_Portada video")[0].play();
+        $(".cmp_Portada video")[0].play();
     }
 }
