c823604f05fecc198857cca5dd31eca44ca71925
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-03-01 17:59:02 -0500

    Update Mobile versión casi Final
    En Mobile faltan ajustes de links de navegación. Y en Escritorio replicar ajustes d UWA en los demas. Ademas implementar diseño tablet.

--- es/webHome/ver_desktop/comp/cmp0_main/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('main').fullpage({
-        sectionSelector: 'section',
-        anchors: ['VideoManifiesto', 'Categorias', 'Equipo', 'Footer'],
-        onLeave: function(index, nextIndex, direction){
-            updateNavStyle(nextIndex)
-        }
-    });
-});
