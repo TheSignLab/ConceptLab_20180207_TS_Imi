639355ebe7db63248b7b264304a32926b45b6447
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-06-12 10:53:42 -0500

    sdf
    sdfsd

--- src/settings.php
+++ src/settings.php
@@ -1,18 +1,36 @@
 <?php
+/*
 if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
     $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     header('HTTP/1.1 301 Moved Permanently');
     header('Location: ' . $redirect);
     exit();
 }
+*/
 ?>
  <?php 
     function ob_html_compress($buf){
         return preg_replace(array('/<!--(.*)-->/Uis',"/[[:blank:]]+/"),array('',' '),str_replace(array("\n","\r","\t"),'',$buf));
     }
 
-    //ob_start("ob_html_compress");
+    ob_start("ob_html_compress");
 
+/*
+
+<VirtualHost *:443>
+     DocumentRoot "C:\Users\walla\Documents\GitHub\ConceptLab_20180207_TS_Imi\src"
+     ServerName dev.thesign
+     ServerAlias *.dev.thesign
+     SSLEngine on
+     SSLCertificateFile "crt/dev.thesign/server.crt"
+     SSLCertificateKeyFile "crt/dev.thesign/server.key"
+     <Directory "C:\Users\walla\Documents\GitHub\ConceptLab_20180207_TS_Imi\src">
+        Options Indexes FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+*/
 ?>
 
     <?php require('backend/database/database_models.php');?>
